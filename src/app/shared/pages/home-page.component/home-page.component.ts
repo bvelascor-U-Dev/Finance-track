39779ea65a28c9ad2f92ddc84fb42328b24bf588
Brayan <bvelascor@unal.edu.co>
2026-07-21 09:31:55 -0500

    Create navbar component

--- src/app/shared/pages/home-page.component/home-page.component.ts
+++ src/app/shared/pages/home-page.component/home-page.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'home-page',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './home-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
